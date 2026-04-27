79ceab8aaf4179bc1037062727a9b4be0fba2498
Suman <sumanmuppavarapu@gmail.com>
2026-04-27 14:57:29 -0500

    init

---
+++ FTC Nexus/FTC_NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTC_NexusApp.swift
+//  FTC Nexus
+//
+//  Created by Suman Muppavarapu on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FTC_NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FTC NexusTests/FTC_NexusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FTC_NexusTests.swift
+//  FTC NexusTests
+//
+//  Created by Suman Muppavarapu on 4/27/26.
+//
+
+import Testing
+@testable import FTC_Nexus
+
+struct FTC_NexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
